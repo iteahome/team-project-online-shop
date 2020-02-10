f67bbb434e56996f890e6d2f1d5d702639b19646
szocsmihai <szocsmihai@gmail.com>
2020-02-10 08:16:19 +0200

    Started work on the UserDAO.updateUser() and UserDAO.deleteUser() methods. Please review.

--- onlineshop/src/main/java/com/shop/dao/UserDAO.java
+++ onlineshop/src/main/java/com/shop/dao/UserDAO.java
@@ -2,18 +2,16 @@ package com.shop.dao;
 
 import com.shop.exception.ShopTechnicalException;
 import com.shop.model.User;
-import com.sun.tools.jdeprscan.scan.Scan;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 /** UserDAO class - allows user database manipulation. */
 
 public class UserDAO {
 
-//  Method to identify all users in the database and store them as objects in an ArrayList:
-    public List<User> findAllUsers() throws ShopTechnicalException {
+/** Method to identify all users in the database and store them as objects in an ArrayList: */
+    public static List<User> findAllUsers() throws ShopTechnicalException {
 
 //      Finding every individual user line (containing user fields' values) from the database:
         String userLine = FileEdit.read("users.txt","");
@@ -29,30 +27,50 @@ public class UserDAO {
         return userList;
     }
 
-//  Method to add new users to the database:
+/** Method to add new users to the database: */
     public static void addUser(String newUserValues) throws ShopTechnicalException {
+
+//      Adding given values to end of the database:
         FileEdit.write("users.txt", newUserValues);
     }
 
-    public static void updateUser(User user) throws ShopTechnicalException { /* UPDATE USER UI NEEDED */
+/** Method to modify current user's values in the database: */
+/* @team: WILL ACTUAL USER OBJECTS BE STORED IN "SECURITY", OR...? THIS METHOD RELIES ON THE ABILITY TO IDENTIFY KNOWN USER OBJECTS */
+    public static void updateUser(User currentUser, String userInput, String newValue) throws ShopTechnicalException {
 
-        /* Find user by asking for credentials confirmation. */
-
-        System.out.println("Please confirm your current email address:");
-        String email = new Scanner(System.in).next();
-        System.out.println("Please confirm your current password:");
-        String password = new Scanner(System.in).next();
-
-        /* Use given credentials to find user in database. */
-
-        String userLine = FileEdit.read("users.txt", email);
-
-        /* Get new values from keyboard. */
-        /* Overwrite user with newly given values */
+//  Setting new user object value according to the user's input:
+    switch (userInput) {
+        case "userName":
+            currentUser.setUserName(newValue);
+            break;
+        case "password":
+            currentUser.setPassword(newValue);
+            break;
+        case "email":
+           currentUser.setEmail(newValue);
+            break;
+        case "phoneNo":
+            currentUser.setPhoneNo(newValue);
+            break;
+        default:
+            System.out.println("Invalid property. Valid properties: userName, password, email, phoneNo.");
     }
 
-//    public void deleteUser(User User) { /*TO BE DEVELOPED*/
-//
-//    }
+//  Identifying old and new value lines:
+/* @team: I AM CONSIDERING EMAIL AS THE UNIQUE IDENTIFIER IN THE DATABASE. PLEASE CONFIRM */
+    String currentUserValues = FileEdit.read("users.txt", currentUser.getEmail());
+    String newUserValues = currentUser.getUserName() + "|" + currentUser.getPassword() + "|" + currentUser.getEmail() + "|" + currentUser.getPhoneNo();
+
+//  Switching the 2 value sets:
+/* @team: WE NEED A FileEdit.replace() METHOD. FileEdit.write() WRITES AT THE END AND ADDS A NEW LINE, WHICH IS NOT WHAT WE NEED HERE. */
+    }
+
+/** Method to delete current user's values from the database: */
+    public void deleteUser(User user) {
+
+//  Removing user line from the database:
+//    FileEdit.delete(user);
+/* @team: WE NEED A FileEdit.delete() METHOD. */
+    }
 
 }
