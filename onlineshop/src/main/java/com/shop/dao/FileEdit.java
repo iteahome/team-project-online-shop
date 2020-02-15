92db5aa0d58d4d5967c8abdc4bfe11e9b4c2cef6
szocsmihai <szocsmihai@gmail.com>
2020-02-15 16:47:14 +0200

    Kept initial version for reading lines in FileEdit.

--- onlineshop/src/main/java/com/shop/dao/FileEdit.java
+++ onlineshop/src/main/java/com/shop/dao/FileEdit.java
@@ -34,62 +34,34 @@ class FileEdit {
 /** Method for reading a file and returning a line of text matching a regex pattern: */
     static String findLine(String filename, String regex) {
 
-//      Initializing the String we are looking for:
+//      Defining a String for the resource file path according to the given fileName:
+        String resourceFile = "./src/main/resources/"+filename;
+//      Initializing a String where the data will be stored:
         String lineData = null;
 
         try {
-//          Pointing to a file that needs to be read:
-            File resourceFile = new File("./src/main/resources/" + filename);
-//          Initializing a way to read the file:
-            Scanner resourceScanner = new Scanner(resourceFile);
-//          Reading the file line by line:
-            while (resourceScanner.hasNextLine()) {
-//              Checking every line for a match to the given regex pattern:
-                if ((lineData = resourceScanner.nextLine()).matches(regex)) {
-//                  When match is found, the line of data is returned:
+//          Reading content from the given file:
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(resourceFile));
+//          Defining regex pattern from the corresponding method parameter (can be empty):
+            Pattern pattern = Pattern.compile(regex);
+
+//          Defining regex matcher (text to be searched) as the current line being read:
+            while ((lineData = bufferedReader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(lineData);
+//              If match is found, line is returned:
+                if (matcher.find()) {
                     return lineData;
                 }
             }
 //          Closing the input stream:
-            resourceScanner.close();
+            bufferedReader.close();
 
-//      If the file is not accessible:
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found.");
+//      If the file can't be read:
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-//      End result of the method - the line of data matching the regex pattern:
-        return  lineData;
-
-/**     INITIAL METHOD - BETTER? WORSE? */
-////      Defining a String for the resource file path according to the given fileName:
-//        String resourceFile = "./src/main/resources/"+filename;
-////      Initializing a String where the data will be stored:
-//        String lineData = null;
-//
-//        try {
-////          Reading content from the given file:
-//            BufferedReader bufferedReader = new BufferedReader(new FileReader(resourceFile));
-////          Defining regex pattern from the corresponding method parameter (can be empty):
-//            Pattern pattern = Pattern.compile(regex);
-//
-////          Defining regex matcher (text to be searched) as the current line being read:
-//            while ((lineData = bufferedReader.readLine()) != null) {
-//                Matcher matcher = pattern.matcher(lineData);
-////              If match is found, line is returned:
-//                if (matcher.find()) {
-//                    return lineData;
-//                }
-//            }
-////          Closing the input stream:
-//            bufferedReader.close();
-//
-////      If the file can't be read:
-//        } catch (IOException e) {
-//            throw new ShopFileException("Error reading file", e);
-//        }
-////      The method's end result - a line of data:
-//        return lineData;
+//      The method's end result - a line of data:
+        return lineData;
 
     }
 
