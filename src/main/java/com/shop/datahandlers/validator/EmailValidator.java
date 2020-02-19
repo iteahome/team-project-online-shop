4993d4b9e19908810da12776c5e20274d500e009
Beni <beniamin.silas@gmail.com>
2020-02-19 21:32:09 +0200

    Added email validator.

---
+++ src/main/java/com/shop/datahandlers/validator/EmailValidator.java
@@ -0,0 +1,21 @@
+package com.shop.datahandlers.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+    private static final String EMAIL_REGEX = "^[\\w-+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+
+    private static Pattern pattern;
+    public Matcher matcher;
+
+    public EmailValidator() {
+        pattern = Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
+    }
+
+
+    public boolean validateEmail(String email) {
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
