ff2d900739226725c252d00dea507a2689f5cb13
Beni <beniamin.silas@gmail.com>
2020-02-20 21:31:42 +0200

    Final version updated regex pattern upgraded

--- src/main/java/com/shop/datahandlers/validator/EmailValidator.java
+++ src/main/java/com/shop/datahandlers/validator/EmailValidator.java
@@ -3,22 +3,15 @@ package com.shop.datahandlers.validator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class EmailValidator {
-    private static final String EMAIL_REGEX = "(.+@.+?:\\..+)";
-
-
-    private static Pattern pattern;
-    public Matcher matcher;
-
-    public EmailValidator() {
-        pattern = Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
-    }
 
 
     public boolean validateEmail(String email) {
-        matcher = pattern.matcher(email);
+        String patternString = ".+@.+?\\..+";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(email);
         return matcher.matches();
+
     }
 }
 
