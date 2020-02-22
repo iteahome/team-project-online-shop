aa0d00c7113e6dba8207c10bb8d0766a6a7e99d6
Bistika <48261571+Bistika@users.noreply.github.com>
2020-02-22 14:24:26 +0200

    slight change to the regex

--- src/main/java/com/shop/datahandlers/validator/PhoneValidator.java
+++ src/main/java/com/shop/datahandlers/validator/PhoneValidator.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class PhoneValidator {
 
     public static boolean validatePhone(String phone) {
-        String patternString = "0040?\\s[0-9]{9}";
+        String patternString = "^0040\\d{9}$";
         Pattern pattern = Pattern.compile(patternString);
         Matcher matcher = pattern.matcher(phone);
         return matcher.matches();
