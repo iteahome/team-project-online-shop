7bd9a552721ea0d7ab94df1bc0d58c9d1d3708ce
szocsmihai <szocsmihai@gmail.com>
2020-02-23 02:03:21 +0200

    Working on UserAccountUI.

--- src/main/java/com/shop/ui/UserAccountUI.java
+++ src/main/java/com/shop/ui/UserAccountUI.java
@@ -6,61 +6,92 @@ import com.shop.service.UserService;
 import com.shop.ui.handlers.InputPopUps;
 import com.shop.ui.handlers.PrintUI;
 
+import static com.shop.ui.handlers.InputPopUps.CANCELLED;
+
 public class UserAccountUI {
 
 
     UserService userService = new UserService();
-    private static final String CANCEL = "NullPointerExceptionFound";
-
 
     void manageAccount(User user) {
-        /* will be called at ShopUI:36 */
-        /* for this user, print details on screen */
-        String userInput = null;
+
         final String VIEW_ACCOUNT_DATA = "1";
         final String MODIFY_ACCOUNT_DATA = "2";
         final String GO_BACK = "0";
 
+        final String CHANGE_USERNAME = "1";
+        final String CHANGE_PASSWORD = "2";
+        final String CHANGE_PHONENO = "3";
+
+        String userInput = null;
+
         do {
-            PrintUI.printBox("View Account Data : 1", "Modify Account Data : 2", "Go Back: 0");
-            userInput = InputPopUps.input("Option:");
+            userInput = InputPopUps.input("View account: 1 | Modify account: 2 | Go Back: 0");
 
             switch (userInput) {
-                case VIEW_ACCOUNT_DATA: {
+                case VIEW_ACCOUNT_DATA:
                     PrintUI.printBox(user.printCompleteUserData());
-                } break;
+                    break;
 
                 case MODIFY_ACCOUNT_DATA: {
-                    final String CHANGE_USERNAME = "1";
-                    final String CHANGE_PASSWORD = "2";
-                    final String CHANGE_PHONENO = "3";
-                    PrintUI.printBox("Change Username : 1", "Change Password: 2", "Change Phone Number: 3");
-                    String dataToChange = InputPopUps.input("Option:");
+                    String dataToChange = InputPopUps.input("Change username: 1 | Change password: 2 | Change phone number: 3");
                     switch (dataToChange) {
-                        case CHANGE_USERNAME: {
-                            user.setUserName(InputPopUps.input("New Username:"));
-                            if (!user.getUserName().equals(CANCEL)) {
-                                try {
-                                    userService.replaceUserData(user);
-                                } catch (ShopException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                        case CHANGE_PASSWORD: {}
-                        case CHANGE_PHONENO: {}
+                        case CHANGE_USERNAME:
+                            changeUserName(user);
+                            break;
+
+                        case CHANGE_PASSWORD:
+                            changePassword(user);
+                            break;
+
+                        case CHANGE_PHONENO:
+                            changePhoneNo(user);
+                            break;
                     }
-                } break;
+                }
+                break;
 
                 case GO_BACK: {
                     break;
-
                 }
+
                 default: {
-                    PrintUI.printBox("Please insert a valid option");
+                    PrintUI.printBox("Please enter a valid option:");
                 }
             }
         } while (!userInput.equals(GO_BACK));
     }
 
+    private void changePhoneNo(User user) {
+        user.setPhoneNo(InputPopUps.input("Enter new phone number:"));
+        if (!user.getPhoneNo().equals(CANCELLED)) {
+            try {
+                userService.replaceUserData(user);
+            } catch (ShopException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void changePassword(User user) {
+        user.setPassword(InputPopUps.input("Enter new password:"));
+        if (!user.getPassword().equals(CANCELLED)) {
+            try {
+                userService.replaceUserData(user);
+            } catch (ShopException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void changeUserName(User user) {
+        user.setUserName(InputPopUps.input("Enter new username:"));
+        if (!user.getUserName().equals(CANCELLED)) {
+            try {
+                userService.replaceUserData(user);
+            } catch (ShopException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
