68f2ee5e695803b6dfe0e7e0dd29af6985b7dbec
Bistika <48261571+Bistika@users.noreply.github.com>
2020-02-24 23:12:27 +0200

    date added to order

--- src/main/java/com/shop/dao/OrderDAO.java
+++ src/main/java/com/shop/dao/OrderDAO.java
@@ -1,17 +1,18 @@
 package com.shop.dao;
 
 import com.shop.exception.ShopTechnicalException;
-import com.shop.model.Product;
 import com.shop.model.User;
 import com.shop.model.Cart;
-
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import java.io.File;
-import java.util.HashMap;
 
 public class OrderDAO {
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
     FileEdit<Cart> fileEdit = new FileEdit<>();
     public void createOrder(Cart cart, User user) throws ShopTechnicalException {
-        String filename = "orders/" + user.getEmail() + System.currentTimeMillis() + ".txt";
+        String filename = "orders/" + user.getEmail() + dtf.format(now) + ".txt";
         File file = new File("./src/main/resources/orders" + filename) ;
         fileEdit.write(filename, cart);
     }
