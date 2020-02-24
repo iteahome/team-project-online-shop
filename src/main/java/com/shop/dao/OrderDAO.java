37089019503b3725291409a64599985231c8e131
Bistika <48261571+Bistika@users.noreply.github.com>
2020-02-24 23:26:48 +0200

    modified dao

--- src/main/java/com/shop/dao/OrderDAO.java
+++ src/main/java/com/shop/dao/OrderDAO.java
@@ -3,17 +3,13 @@ package com.shop.dao;
 import com.shop.exception.ShopTechnicalException;
 import com.shop.model.User;
 import com.shop.model.Cart;
-import java.time.format.DateTimeFormatter;
-import java.time.LocalDateTime;
 import java.io.File;
 
 public class OrderDAO {
-    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-    LocalDateTime now = LocalDateTime.now();
     FileEdit<Cart> fileEdit = new FileEdit<>();
     public void createOrder(Cart cart, User user) throws ShopTechnicalException {
-        String filename = "orders/" + user.getEmail() + dtf.format(now) + ".txt";
-        File file = new File("./src/main/resources/orders" + filename) ;
-        fileEdit.write(filename, cart);
+        String filename = user.getEmail() + java.time.LocalDate.now()+ System.currentTimeMillis() + ".txt";
+        File file = new File("./src/main/resources/orders/" + filename) ;
+        fileEdit.write("orders/"+filename, cart);
     }
 }
